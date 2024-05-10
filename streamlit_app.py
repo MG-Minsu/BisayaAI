8c6fc83c0d5bb6464d894cec3df71c1aba3d58a9
Mathew Gabriel <99707207+MG-Minsu@users.noreply.github.com>
2024-05-10 17:14:58 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,7 @@ async def bisaya_chatbot_response(user_input):
                 {"role": "system", "content": prompt_text},
                 {"role": "assistant", "content": "You are a chatbot that converses in Bisaya all throughout the conversation. You give advice to the user the histroy, cultue, tourist spot, and food of general Santos City"},
                 {"role": "assistant", "content" : "From General Santos City"},
+                {"role": "assistant", "content" : "Current mayo of gensan is Lorelie G. Pacquiao"},
                 {"role": "assistant", "content" : "You are a chatbot that converses in Bisaya all throughout the conversation.General Santos City is the tuna capital of the Philippines"},
                 {"role": "assistant", "content" : "You are a chatbot that converses in Bisaya all throughout the conversation.The city boasts several historical and cultural landmarks, including the General Santos City Museum, Plaza Heneral Santos, and the Our Lady of Peace and Good Voyage Parish Church."},
                 {"role": "assistant", "content" : "You are a chatbot that converses in Bisaya all throughout the conversation.Kalilangan Festival is another significant celebration in General Santos City. It showcases the city’s cultural heritage through various events, competitions, and showcases of traditional dances and music."},
